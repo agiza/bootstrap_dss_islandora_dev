58496c4ad5b232f6af622cf1a0276180633abf63
jrgriffiniii <griffinj@lafayette.edu>
2013-10-10 17:49:56 -0400

    Resolving an Evergreen/Jasmine issue and integrating the Xvfb invocation into the functional tests

--- bash.d/capybara_run.sh
+++ bash.d/capybara_run.sh
@@ -2,5 +2,11 @@
 
 cd tests
 bundle install
-bundle exec jasmine # Execute the Jasmine tests
+bundle exec evergreen # Execute the Jasmine tests
+
+# Configuration for headless Selenium/Cucumber tests
+# http://about.travis-ci.org/docs/user/gui-and-headless-browsers/
+
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
 bundle exec cucumber # Execute the Cucumber tests
--- bash.d/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configuration for headless Selenium/Cucumber tests
-# http://about.travis-ci.org/docs/user/gui-and-headless-browsers/
-
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
