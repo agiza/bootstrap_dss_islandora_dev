ff5a3f69fbb640190a4003dead7a391f92d36419
jrgriffiniii <griffinj@lafayette.edu>
2013-11-07 16:37:32 -0500

    DSSSM-184 Placing the "contact" region template under version control

---
+++ templates/region--contact.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+  /**
+   * @file
+   * For theming the contact region
+   *
+   */
+?>
+
+<?php if ($content): ?>
+
+  <div class="lafayette-dss-modal" id="contact-modal" tabindex="-1" role="dialog" aria-labelledby="Contact Interface" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	  <h4 class="modal-title">Contact DSS</h4>
+        </div>
+        <div class="modal-body">
+
+          <?php print render($content); ?>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+<?php endif; ?>
